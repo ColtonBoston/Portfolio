b7af4fd2f7a23ed1d6b03a331a13888d65196f0d
ColtonBoston <cbos316@gmail.com>
2016-06-03 13:38:44 -0500

    updated portfolio

---
+++ functions.js
@@ -0,0 +1,21 @@
+/*global $, jQuery, alert */
+$(document).ready(function () {
+    
+$('#back-arrow').click(function () {
+    "use strict";
+    $('#work-belt').css('left', '0');
+    $('.project').fadeOut();
+});
+
+$('.project-unit').click(function () {
+    "use strict";
+    var project = this.id;
+    $('#work-belt').css('left', '-100%');
+    console.log(project);
+    if (project.match(/simon/))
+        $('#simon').delay(150).fadeIn();
+    else
+        $('#tictactoe').delay(150).fadeIn();
+});
+    
+});
